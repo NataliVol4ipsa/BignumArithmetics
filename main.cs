816b973ffd6f5ddc6caf2345a154cab9c24cbfdf
NataliVol4ica <natvol4ica@gmail.com>
2018-07-03 13:33:36 +0300

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace net.NataliVol4ica.BignumArithmetics
+{
+    public static class BignumArithmetics
+    {
+        public static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
