bbbcd49d8dbec18e97fba62474746f8928a91d63
NataliVol4ica <natvol4ica@gmail.com>
2018-07-03 22:39:27 +0300

    branches testing

--- main.cs
+++ main.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-/// 
 namespace net.NataliVol4ica.BignumArithmetics
 {
     public static class BignumArithmetics
     {
         public static int Main(string[] args)
         {
+            //Console.Read();
             return 0;
         }
     }
