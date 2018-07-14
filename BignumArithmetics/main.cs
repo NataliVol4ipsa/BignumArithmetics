98151d13d02e4ed7170a17461446346471893c59
NataliVol4ica <natvol4ica@gmail.com>
2018-07-15 01:37:18 +0300

    really correct mul!

--- BignumArithmetics/main.cs
+++ BignumArithmetics/main.cs
@@ -5,26 +5,42 @@ using System.Collections.Generic;
 namespace BignumArithmetics
 {
     public static class BignumArithmetics
-    {
-        /*
+    {        
         static int errors = 0;
-        static void DoTesting(string left, string right, string sum, string dif)
+        static void DoTesting(string left, string right, string sum, string dif, string mul)
         {
             BigFloat A = BigFloat.CreateFromString(left);
             BigFloat B = BigFloat.CreateFromString(right);
 
             BigFloat C = A + B;
             BigFloat D = A - B;
-            //Console.WriteLine("   {0}\n + {1}", left, right);
-            //Console.WriteLine(C);
-            //Console.WriteLine(sum);
-            //Console.WriteLine("   {0}\n - {1}", left, right);
-            //Console.WriteLine(D);
-            //Console.WriteLine(dif);
+            BigFloat E = A * B;
+            
+           
             if (string.Compare(C.ToString(), sum) != 0)
+            {
+                Console.WriteLine("   {0}\n + {1}", left, right);
+                Console.WriteLine(C);
+                Console.WriteLine(sum);
                 errors++;
+            }
+
             if (string.Compare(D.ToString(), dif) != 0)
+            {
+                Console.WriteLine("   {0}\n + {1}", left, right);
+                Console.WriteLine(D);
+                Console.WriteLine(dif);
                 errors++;
+            }
+
+            if (string.Compare(E.ToString(), mul) != 0)
+            {
+                Console.WriteLine("   {0}\n + {1}", left, right);
+                Console.WriteLine(E);
+                Console.WriteLine(mul);
+                errors++;
+            }
+
         }
         static void Test()
         {
@@ -39,14 +55,16 @@ namespace BignumArithmetics
             B /= 100000;
             decimal C = A + B;
             decimal D = A - B;
+            decimal E = A * B;
 
             DoTesting(A.ToString().Replace(",", "."),
                 B.ToString().Replace(",", "."),
                 C.ToString().Replace(",", "."),
-                D.ToString().Replace(",", "."));
-        }*/
+                D.ToString().Replace(",", "."),
+                E.ToString().Replace(",", "."));
+        }
 
-        static void Test()
+        /*static void Test()
         {
             BigFloat A = BigFloat.CreateFromNumber(12.4f);
             BigFloat B = BigFloat.CreateFromNumber(0.25f);
@@ -54,19 +72,20 @@ namespace BignumArithmetics
             Console.WriteLine(A);
             Console.WriteLine(B);
             Console.WriteLine(C);
-        }
+        }*/
 
         public static int Main(string[] args)
         {
             try
             {
-                Test();
+                for (int i = 0; i < 1000; i++)
+                    Test();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-            //Console.WriteLine("Fails : " + errors);
+            Console.WriteLine("Fails : " + errors);
             Console.Read();
             return 0;
         }
